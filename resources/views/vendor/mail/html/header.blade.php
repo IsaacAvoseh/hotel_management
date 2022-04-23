13f809ba06095ecd17e95711b525884209c3f0c1
Isaac Avoseh <53424351+IsaacAvoseh@users.noreply.github.com>
2022-04-23 15:39:21 +0100

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
+            @if (trim($slot) === 'Hotel')
             <img src="https://lh3.googleusercontent.com/a-/AOh14Ggoa3XowMBbzuQeICcMUi0RRhUp2gxA3FV0u4OJSw=s96-c-rg-br100" class="logo" alt="Hotel Logo">
             @else
             {{ $slot }}
